a28cdecb190fdcecfe3cdf3097c136f8417cf178
Antoi <antoine.lafitte@etu.univ-amu.fr>
2026-01-27 12:04:12 +0100

    rudimentary draw area - testing Qt's draw system

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 
+#include "renderarea.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setupUi(this);
+    RenderArea *ra = new RenderArea();
+    this->setCentralWidget(ra);
 }
 
 MainWindow::~MainWindow() {}
