0bdb97dac73e64e288f98b3dd5667edd68bb43da
c21218805 <maelle.croguennec@etu.univ-amu.fr>
2026-02-10 10:02:18 +0100

    Open Save and Save As buttons

--- mainwindow.h
+++ mainwindow.h
@@ -1,16 +1,41 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "ui_mainwindow.h"
-
 #include <QMainWindow>
+#include <QImage>
 
-class MainWindow : public QMainWindow, private Ui::MainWindow
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void onActionOpenTriggered();
+    void onActionSaveTriggered();
+    void onActionSaveAsTriggered();
+    void onActionCloseProjectTriggered();
+
+private:
+    bool saveToFile(const QString &fileName);
+    bool exportImage(const QString &fileName);
+    bool maybeSave();
+    void closeCurrentDocument();
+    QString forceExtension(const QString &fileName, const QString &ext);
+
+private:
+    Ui::MainWindow *ui;
+
+    QImage  m_image;
+    QString m_currentFilePath;
+    bool    m_isModified = false;
+    bool    m_hasDocument = false;
 };
+
 #endif // MAINWINDOW_H
