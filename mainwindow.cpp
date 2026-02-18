4ad8d97f7b0b04d556d72a97f24a7d70d24dbb9e
shellraiser <mehdi.belhachemi@etu.univ-amu.fr>
2026-02-18 12:59:41 +0100

    mehdi : correction de la toolBar et ajout d'un scrollbar qui marche bien

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,45 @@
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 
+#include <QScrollArea>
+#include <QHBoxLayout>
 #include "renderarea.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
-    setupUi(this);
+    ui->setupUi(this);
 
+    QWidget *toolWidget = ui->widget;
+
+    toolWidget->setFixedWidth(80);
+    toolWidget->setMinimumHeight(900);
+    //je peux changer la taille si necessaire
+
+    QScrollArea *scrollArea = new QScrollArea;
+    scrollArea->setWidget(toolWidget);
+    scrollArea->setWidgetResizable(false);
+
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+
+    scrollArea->setFixedWidth(95);
+
+
+    RenderArea *renderArea = new RenderArea;
+
+    QHBoxLayout *mainLayout = new QHBoxLayout(ui->centralwidget);
+    mainLayout->setSpacing(0);
+    mainLayout->setContentsMargins(0,0,0,0);
+
+
+    mainLayout->addWidget(scrollArea);
+    mainLayout->addWidget(renderArea);
 }
 
-MainWindow::~MainWindow() {}
-
-
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
