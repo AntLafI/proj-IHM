d35db226ffe01429092b4781d3fdd642defa9026
c21218805 <maelle.croguennec@etu.univ-amu.fr>
2026-02-21 02:12:15 +0100

    Correct some minor bugs

--- renderarea.h
+++ renderarea.h
@@ -2,20 +2,17 @@
 #define RENDERAREA_H
 
 #include "ui_renderarea.h"
-#include <QGraphicsView>
-#include <QGraphicsScene>
-#include <QGraphicsItem>
-#include <QGraphicsPolygonItem>
-#include <QGraphicsEllipseItem>
-#include <QGraphicsRectItem>
-#include <QGraphicsPixmapItem>
+#include <QWidget>
 #include <QMouseEvent>
 #include <QPen>
+#include <QPainter>
 #include <QImage>
+#include <QPolygon>
 #include <QVector>
 #include <QSize>
+#include <QRect>
 
-class RenderArea : public QGraphicsView, private Ui::RenderArea
+class RenderArea : public QWidget, private Ui::RenderArea
 {
     Q_OBJECT
 
@@ -29,7 +26,7 @@ public:
     bool canUndo() const;
     bool canRedo() const;
     void renderToPainter(QPainter *p, const QSize &size) const;
-    void setTool(Tool t);
+    void setTool(Tool t){ m_tool = t; }
 
 public slots:
     void undo();
@@ -44,31 +41,46 @@ protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
 
 private:
-    QPen currentPen;
+    QPainter painter;
+    QPen pen;
+
+    QImage m_background;
+
+    QPolygon m_currentStroke;
+    QVector<QPolygon> m_strokes;
+    QVector<QPolygon> m_redoStrokes;
+
+    QRect m_currentCircle;
+    QVector<QRect> m_circles;
+    QVector<QRect> m_redoCircles;
+
+    QRect m_currentRect;
+    QVector<QRect> m_rects;
+    QVector<QRect> m_redoRects;
+
     Tool m_tool = Freehand;
 
-    QGraphicsPolygonItem *m_currentPoly = nullptr;
-    QVector<QPointF> m_currentPolyPts;
+    QSize m_baseSize;
 
-    QGraphicsEllipseItem *m_currentEllipse = nullptr;
-    QGraphicsRectItem   *m_currentRect = nullptr;
-    QPointF m_pressScene;
+    QPoint m_pressPos;
 
-    QGraphicsPixmapItem *m_bgItem = nullptr;
-
-    struct Op {
-        enum Kind { AddItem, RemoveItem } kind;
-        QGraphicsItem *item = nullptr;
+    struct EraseOp {
+        enum Kind { Stroke, Circle, Rect } kind;
+        int index = -1;
+        QPolygon poly;
+        QRect rect;
     };
-    QVector<Op> m_undo;
-    QVector<Op> m_redo;
+    QVector<EraseOp> m_eraseHistory;
+    QVector<EraseOp> m_eraseRedo;
 
-    void pushAdd(QGraphicsItem *it);
-    void pushRemove(QGraphicsItem *it);
+    bool m_lastCanUndo = false;
+    bool m_lastCanRedo = false;
+
     void notifyState();
-    void eraseAt(const QPoint &viewPos);
+    void eraseAt(const QPoint &pt);
 };
 
 #endif // RENDERAREA_H
